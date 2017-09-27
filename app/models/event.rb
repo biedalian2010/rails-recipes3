45620e403c55d6b7cab4308f97f53eb8408350a8
李阳 <11@11.com>
2017-09-27 22:44:46 +0800

    Multi-language settiongs

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,11 @@
 class Event < ApplicationRecord
 
- validates_presence_of :name, :friendly_id
+ validates_presence_of :name, :friendly_id                      #在name生效前,需执行friendly_id
 
- validates_uniqueniess_of :friendly_id
- validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
+ validates_uniqueness_of :friendly_id                          #friendly_id 必须保持唯一
+ validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/  #格式只限小写英文数字及横线
+
+ before_validation :generate_friendly_id, :on => :create
 
  def to_param
    self.friendly_id
